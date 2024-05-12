32771e622364240edff47e4be49317c98369f5c5
kodaimura <kodaimura123@gmail.com>
2024-05-12 19:32:14 +0900

    recursively

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import os
 import fitz
 
-doc = fitz.open('input.pdf') # ドキュメントを開く
-out = open("output.txt", "wb") # テキスト出力を作成する
-for page in doc: # ドキュメントのページを反復処理する
-    text = page.get_text().encode("utf8") # プレーンテキストを取得する（UTF-8形式）
-    out.write(text) # ページのテキストを書き込む
-    out.write(bytes((12,))) # ページ区切り（フォームフィード0x0C）を書き込む
+def separate_extension(file_name):
+    base_name = os.path.splitext(file_name)[0]
+    extension = os.path.splitext(file_name)[1]
+    
+    return base_name, extension
+
+
+def is_skip_file(file_name):
+    return file_name in [".gitignore", ".DS_Store"]
+
+
+for root, dirs, files in  os.walk("input"):
+    os.makedirs(f"output/{root[6:]}", exist_ok=True)
+    for file_name in files:
+        if (is_skip_file(file_name)): continue
+
+        file_path = os.path.join(root, file_name)
+        base_name, extension = separate_extension(file_name)
+        doc = fitz.open(file_path)
+        out = open(f"output/{root[6:]}/{base_name}.txt", "wb")
+        for page in doc:
+            text = page.get_text().encode("utf8")
+            out.write(text)
+            out.write(bytes((12,)))
 out.close()
+
